a5508cc815cad661bb9599f7df4dab194d661da6
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2014-04-12 18:41:52 -0400

    Add license headers.

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
-# This file is in the public domain
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3, as published 
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
 ###################### DO NOT TOUCH THIS (HEAD TO THE SECOND PART) ######################
@@ -126,12 +136,12 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 DistUtilsExtra.auto.setup(
     name='digitalocean-indicator',
     version='0.1',
-#    license='GPL-3',
-#    author='Andrew Starr-Bochicchio',
-#    author_email='a.starr.b@gmail.com',
+    license='GPL-3',
+    author='Andrew Starr-Bochicchio',
+    author_email='a.starr.b@gmail.com',
     #description='UI for managing …',
     #long_description='Here a longer description',
-    #url='https://launchpad.net/digitalocean-indicator',
+    url='https://launchpad.net/digitalocean-indicator',
     cmdclass={'install': InstallAndUpdateDataDirectory}
     license='GPL-3',
     )
