14172ac4acb8107657555613d348a21a04002747
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2014-04-13 10:50:24 -0400

    Add support for autostarting on login.

--- digitalocean_indicator/PreferencesDigitaloceanIndicatorDialog.py
+++ digitalocean_indicator/PreferencesDigitaloceanIndicatorDialog.py
@@ -19,14 +19,21 @@
 # data/glib-2.0/schemas/net.launchpad.digitalocean-indicator.gschema.xml
 # See http://developer.gnome.org/gio/stable/GSettings.html for more info.
 
-from gi.repository import Gio # pylint: disable=E0611
+from gi.repository import GLib, Gio # pylint: disable=E0611
 
 from locale import gettext as _
 
-import logging
+import logging, os
 logger = logging.getLogger('digitalocean_indicator')
 
 from digitalocean_indicator_lib.PreferencesDialog import PreferencesDialog
+from digitalocean_indicator_lib.helpers import get_media_file
+
+autostart_dir = os.path.join(GLib.get_user_config_dir(),"autostart/")
+autostart_template = "digitalocean-indicator-autostart.desktop"
+autostart_file = get_media_file(autostart_template)
+autostart_file = autostart_file.replace("file:///", '')
+installed_file = os.path.join(autostart_dir, autostart_template)
 
 class PreferencesDigitaloceanIndicatorDialog(PreferencesDialog):
     __gtype_name__ = "PreferencesDigitaloceanIndicatorDialog"
@@ -43,5 +50,17 @@ class PreferencesDigitaloceanIndicatorDialog(PreferencesDialog):
         settings.bind("do-client-id", do_client_id, "text", Gio.SettingsBindFlags.DEFAULT)
         refresh_interval = self.builder.get_object('refresh_interval_spin')
         settings.bind("refresh-interval", refresh_interval, "value", Gio.SettingsBindFlags.DEFAULT)
-        # Code for other initialization actions should be added here.
 
+        self.autostart_switch = builder.get_object("autostart_switch")
+        if os.path.isfile(installed_file):
+            self.autostart_switch.set_active(True)
+        self.autostart_switch.connect('notify::active', self.on_autostart_switch_activate)
+
+    def on_autostart_switch_activate(self, widget, data=None):
+        if self.autostart_switch.get_active():
+            if not os.path.exists(autostart_dir):
+                os.mkdir(autostart_dir)
+            if os.path.isdir(autostart_dir):
+                os.symlink(autostart_file, installed_file)
+        else:
+            os.unlink(installed_file)
